ce09b6fefd577184746511b038158e2cd96651c6
Alexander <a.gutsol@gmail.com>
2023-08-09 16:33:50 +0300

    Java Dev HW Module 13 - 1st Version

--- src/main/java/org/gs/todoList/TodoListApplication.java
+++ src/main/java/org/gs/todoList/TodoListApplication.java
@@ -1,7 +1,5 @@
 package org.gs.todoList;
 
-import org.gs.todoList.entity.Note;
-import org.gs.todoList.service.NoteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
